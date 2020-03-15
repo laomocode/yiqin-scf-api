29c22dc3db608174e6063f460d4c3676288f23e6
laomocode <3344907598@qq.com>
2020-03-15 14:12:39 +0800

    更改数据获取链接

--- guowai/index.py
+++ guowai/index.py
@@ -2,7 +2,7 @@ from requests import get
 from bs4 import BeautifulSoup
 from json import loads
 def main_handler(event, context):
-    content=get("https://3g.dxy.cn/newh5/view/pneumonia")
+    content=get("https://ncov.dxy.cn/ncovh5/view/pneumonia")
     content.encoding='utf-8'
     data=content.text
     bs = BeautifulSoup(data,'html.parser')
